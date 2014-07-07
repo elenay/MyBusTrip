06c580d75b5b5ef68cb629a3fe1f6ba7a35be90e
elenay <elena.yermakova@chloeandisabel.com>
2014-07-07 23:56:11 +0300

    rails app

---
+++ busapp/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Busapp::Application.config.secret_key_base = '07174936a0a3af8152f03423504641f18d8ee83ee4bb1c3e0c32c1c65c32404a693da0096abc6c4ec9fafaa18ee400136ea5f59d1a4a6bce1c790f8baf79dacd'
